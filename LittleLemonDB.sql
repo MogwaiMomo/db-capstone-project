3f5842e60ad430a88dc442a12c4f50db1e5322da
MogwaiMomo <momokoprice@gmail.com>
2024-03-31 21:41:05 -0400

    further normalization

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -32,9 +32,18 @@ CREATE TABLE `bookings` (
   KEY `fk_Bookings_Customers_idx` (`Customers_CustomerID`),
   KEY `fk_Bookings_Staff1_idx` (`Staff_EmployeeID`),
   CONSTRAINT `fk_Bookings_Customers` FOREIGN KEY (`Customers_CustomerID`) REFERENCES `customers` (`CustomerID`),
-  CONSTRAINT `fk_Bookings_Staff1` FOREIGN KEY (`Staff_EmployeeID`) REFERENCES `staff` (`EmployeeID`)
+  CONSTRAINT `fk_Bookings_Staff1` FOREIGN KEY (`Staff_EmployeeID`) REFERENCES `employees` (`EmployeeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookings`
+--
+
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `customers`
@@ -53,6 +62,15 @@ CREATE TABLE `customers` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `deliveries`
 --
 
@@ -62,13 +80,47 @@ DROP TABLE IF EXISTS `deliveries`;
 CREATE TABLE `deliveries` (
   `DeliveryID` int NOT NULL AUTO_INCREMENT,
   `DeliveryDate` varchar(45) DEFAULT NULL,
-  `DeliveryStatus` varchar(45) DEFAULT NULL,
+  `DeliveryCost` decimal(5,2) DEFAULT NULL,
   `Locations_LocationID` int NOT NULL,
+  `DeliveryStatuses_DeliveryStatuses` varchar(45) NOT NULL,
   PRIMARY KEY (`DeliveryID`),
   KEY `fk_Deliveries_Locations1_idx` (`Locations_LocationID`),
+  KEY `fk_Deliveries_DeliveryStatuses1_idx` (`DeliveryStatuses_DeliveryStatuses`),
+  CONSTRAINT `fk_Deliveries_DeliveryStatuses1` FOREIGN KEY (`DeliveryStatuses_DeliveryStatuses`) REFERENCES `deliverystatuses` (`DeliveryStatuses`),
   CONSTRAINT `fk_Deliveries_Locations1` FOREIGN KEY (`Locations_LocationID`) REFERENCES `locations` (`LocationID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deliveries`
+--
+
+LOCK TABLES `deliveries` WRITE;
+/*!40000 ALTER TABLE `deliveries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deliveries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deliverystatuses`
+--
+
+DROP TABLE IF EXISTS `deliverystatuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `deliverystatuses` (
+  `DeliveryStatuses` varchar(45) NOT NULL,
+  PRIMARY KEY (`DeliveryStatuses`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deliverystatuses`
+--
+
+LOCK TABLES `deliverystatuses` WRITE;
+/*!40000 ALTER TABLE `deliverystatuses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deliverystatuses` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `employeeroles`
@@ -78,12 +130,20 @@ DROP TABLE IF EXISTS `employeeroles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `employeeroles` (
-  `EmployeeRoleID` int NOT NULL AUTO_INCREMENT,
-  `EmployeeRole` varchar(45) DEFAULT NULL,
-  `EmployeeSalary` int DEFAULT NULL,
-  PRIMARY KEY (`EmployeeRoleID`)
+  `EmployeeRole` varchar(45) NOT NULL,
+  `EmployeeSalary` int NOT NULL,
+  PRIMARY KEY (`EmployeeRole`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employeeroles`
+--
+
+LOCK TABLES `employeeroles` WRITE;
+/*!40000 ALTER TABLE `employeeroles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employeeroles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `employees`
@@ -95,40 +155,44 @@ DROP TABLE IF EXISTS `employees`;
 CREATE TABLE `employees` (
   `EmployeeID` int NOT NULL AUTO_INCREMENT,
   `EmployeeName` varchar(255) DEFAULT NULL,
-  `EmployeeRoles_EmployeeRoleID` int NOT NULL,
+  `EmployeeRoles_EmployeeRole` varchar(45) NOT NULL,
   PRIMARY KEY (`EmployeeID`),
-  KEY `fk_Employees_EmployeeRoles1_idx` (`EmployeeRoles_EmployeeRoleID`),
-  CONSTRAINT `fk_Employees_EmployeeRoles1` FOREIGN KEY (`EmployeeRoles_EmployeeRoleID`) REFERENCES `employeeroles` (`EmployeeRoleID`)
+  KEY `fk_Employees_EmployeeRoles1_idx` (`EmployeeRoles_EmployeeRole`),
+  CONSTRAINT `fk_Employees_EmployeeRoles1` FOREIGN KEY (`EmployeeRoles_EmployeeRole`) REFERENCES `employeeroles` (`EmployeeRole`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `locationcities`
+-- Dumping data for table `employees`
 --
 
-DROP TABLE IF EXISTS `locationcities`;
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locationregions`
+--
+
+DROP TABLE IF EXISTS `locationregions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `locationcities` (
-  `LocationCityID` int NOT NULL AUTO_INCREMENT,
-  `LocationCity` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`LocationCityID`)
+CREATE TABLE `locationregions` (
+  `LocationCountry` varchar(100) NOT NULL,
+  `LocationCity` varchar(100) NOT NULL,
+  PRIMARY KEY (`LocationCity`,`LocationCountry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `locationcountries`
+-- Dumping data for table `locationregions`
 --
 
-DROP TABLE IF EXISTS `locationcountries`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `locationcountries` (
-  `LocationCountryID` int NOT NULL AUTO_INCREMENT,
-  `LocationCountry` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`LocationCountryID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `locationregions` WRITE;
+/*!40000 ALTER TABLE `locationregions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `locationregions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `locations`
@@ -140,11 +204,22 @@ DROP TABLE IF EXISTS `locations`;
 CREATE TABLE `locations` (
   `LocationID` int NOT NULL AUTO_INCREMENT,
   `LocationAddress` varchar(255) DEFAULT NULL,
-  `LocationCity` varchar(45) DEFAULT NULL,
-  `LocationCountry` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`LocationID`)
+  `LocationRegions_LocationCity` varchar(100) NOT NULL,
+  `LocationRegions_LocationCountry` varchar(100) NOT NULL,
+  PRIMARY KEY (`LocationID`),
+  KEY `fk_Locations_LocationRegions1_idx` (`LocationRegions_LocationCity`,`LocationRegions_LocationCountry`),
+  CONSTRAINT `fk_Locations_LocationRegions1` FOREIGN KEY (`LocationRegions_LocationCity`, `LocationRegions_LocationCountry`) REFERENCES `locationregions` (`LocationCity`, `LocationCountry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `menu_orders`
@@ -154,14 +229,45 @@ DROP TABLE IF EXISTS `menu_orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `menu_orders` (
+  `MenuItems_MenuItemName` varchar(100) NOT NULL,
   `Orders_OrderID` varchar(11) NOT NULL,
-  `Menu_MenuID` int NOT NULL,
-  KEY `fk_Menu_Orders_Menu1_idx` (`Menu_MenuID`),
+  KEY `fk_Menu_Orders_MenuItems1_idx` (`MenuItems_MenuItemName`),
   KEY `fk_Menu_Orders_Orders1_idx` (`Orders_OrderID`),
-  CONSTRAINT `fk_Menu_Orders_Menu1` FOREIGN KEY (`Menu_MenuID`) REFERENCES `menuitems` (`MenuItemID`),
+  CONSTRAINT `fk_Menu_Orders_MenuItems1` FOREIGN KEY (`MenuItems_MenuItemName`) REFERENCES `menuitems` (`MenuItemName`),
   CONSTRAINT `fk_Menu_Orders_Orders1` FOREIGN KEY (`Orders_OrderID`) REFERENCES `orders` (`OrderID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_orders`
+--
+
+LOCK TABLES `menu_orders` WRITE;
+/*!40000 ALTER TABLE `menu_orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menuitemcuisines`
+--
+
+DROP TABLE IF EXISTS `menuitemcuisines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menuitemcuisines` (
+  `MenuItemCuisine` varchar(100) NOT NULL,
+  PRIMARY KEY (`MenuItemCuisine`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menuitemcuisines`
+--
+
+LOCK TABLES `menuitemcuisines` WRITE;
+/*!40000 ALTER TABLE `menuitemcuisines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menuitemcuisines` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `menuitems`
@@ -171,13 +277,47 @@ DROP TABLE IF EXISTS `menuitems`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `menuitems` (
-  `MenuItemID` int NOT NULL AUTO_INCREMENT,
-  `MenuItemName` varchar(100) DEFAULT NULL,
-  `MenuItemType` varchar(100) DEFAULT NULL,
-  `MenuItemCuisine` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`MenuItemID`)
+  `MenuItemName` varchar(100) NOT NULL,
+  `MenuItemTypes_MenuItemType` varchar(100) NOT NULL,
+  `MenuItemCuisines_MenuItemCuisine` varchar(100) NOT NULL,
+  PRIMARY KEY (`MenuItemName`),
+  KEY `fk_MenuItems_MenuItemTypes1_idx` (`MenuItemTypes_MenuItemType`),
+  KEY `fk_MenuItems_MenuItemCuisines1_idx` (`MenuItemCuisines_MenuItemCuisine`),
+  CONSTRAINT `fk_MenuItems_MenuItemCuisines1` FOREIGN KEY (`MenuItemCuisines_MenuItemCuisine`) REFERENCES `menuitemcuisines` (`MenuItemCuisine`),
+  CONSTRAINT `fk_MenuItems_MenuItemTypes1` FOREIGN KEY (`MenuItemTypes_MenuItemType`) REFERENCES `menuitemtypes` (`MenuItemType`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menuitems`
+--
+
+LOCK TABLES `menuitems` WRITE;
+/*!40000 ALTER TABLE `menuitems` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menuitems` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menuitemtypes`
+--
+
+DROP TABLE IF EXISTS `menuitemtypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menuitemtypes` (
+  `MenuItemType` varchar(100) NOT NULL,
+  PRIMARY KEY (`MenuItemType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menuitemtypes`
+--
+
+LOCK TABLES `menuitemtypes` WRITE;
+/*!40000 ALTER TABLE `menuitemtypes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menuitemtypes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `orders`
@@ -188,8 +328,10 @@ DROP TABLE IF EXISTS `orders`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
   `OrderID` varchar(11) NOT NULL,
-  `Quantity` varchar(45) DEFAULT NULL,
-  `OrderCost` varchar(45) DEFAULT NULL,
+  `Quantity` int DEFAULT NULL,
+  `OrderCost` decimal(6,2) DEFAULT NULL,
+  `OrderSales` decimal(6,2) DEFAULT NULL,
+  `Discount` decimal(5,2) DEFAULT NULL,
   `Deliveries_DeliveryID` int NOT NULL,
   `Customers_CustomerID` varchar(11) NOT NULL,
   PRIMARY KEY (`OrderID`),
@@ -201,20 +343,13 @@ CREATE TABLE `orders` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `staff`
+-- Dumping data for table `orders`
 --
 
-DROP TABLE IF EXISTS `staff`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `staff` (
-  `EmployeeID` int NOT NULL AUTO_INCREMENT,
-  `EmployeeName` varchar(255) DEFAULT NULL,
-  `EmployeeRole` varchar(45) DEFAULT NULL,
-  `EmployeeSalary` int DEFAULT NULL,
-  PRIMARY KEY (`EmployeeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -225,4 +360,4 @@ CREATE TABLE `staff` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-31 21:05:52
+-- Dump completed on 2024-03-31 21:40:11
--- Week1_Task2_fwd_engineer.sql
+++ Week1_Task2_fwd_engineer.sql
@@ -30,10 +30,9 @@ ENGINE = InnoDB;
 -- Table `LittleLemonDB`.`EmployeeRoles`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`EmployeeRoles` (
-  `EmployeeRoleID` INT NOT NULL AUTO_INCREMENT,
-  `EmployeeRole` VARCHAR(45) NULL,
-  `EmployeeSalary` INT NULL,
-  PRIMARY KEY (`EmployeeRoleID`))
+  `EmployeeRole` VARCHAR(45) NOT NULL,
+  `EmployeeSalary` INT NOT NULL,
+  PRIMARY KEY (`EmployeeRole`))
 ENGINE = InnoDB;
 
 
@@ -43,12 +42,12 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Employees` (
   `EmployeeID` INT NOT NULL AUTO_INCREMENT,
   `EmployeeName` VARCHAR(255) NULL,
-  `EmployeeRoles_EmployeeRoleID` INT NOT NULL,
+  `EmployeeRoles_EmployeeRole` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`EmployeeID`),
-  INDEX `fk_Employees_EmployeeRoles1_idx` (`EmployeeRoles_EmployeeRoleID` ASC) VISIBLE,
+  INDEX `fk_Employees_EmployeeRoles1_idx` (`EmployeeRoles_EmployeeRole` ASC) VISIBLE,
   CONSTRAINT `fk_Employees_EmployeeRoles1`
-    FOREIGN KEY (`EmployeeRoles_EmployeeRoleID`)
-    REFERENCES `LittleLemonDB`.`EmployeeRoles` (`EmployeeRoleID`)
+    FOREIGN KEY (`EmployeeRoles_EmployeeRole`)
+    REFERENCES `LittleLemonDB`.`EmployeeRoles` (`EmployeeRole`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -80,22 +79,12 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `LittleLemonDB`.`LocationCities`
+-- Table `LittleLemonDB`.`LocationRegions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`LocationCities` (
-  `LocationCityID` INT NOT NULL AUTO_INCREMENT,
-  `LocationCity` VARCHAR(100) NULL,
-  PRIMARY KEY (`LocationCityID`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`LocationCountries`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`LocationCountries` (
-  `LocationCountryID` INT NOT NULL AUTO_INCREMENT,
-  `LocationCountry` VARCHAR(100) NULL,
-  PRIMARY KEY (`LocationCountryID`))
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`LocationRegions` (
+  `LocationCountry` VARCHAR(100) NOT NULL,
+  `LocationCity` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`LocationCity`, `LocationCountry`))
 ENGINE = InnoDB;
 
 
@@ -105,21 +94,24 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Locations` (
   `LocationID` INT NOT NULL AUTO_INCREMENT,
   `LocationAddress` VARCHAR(255) NULL,
-  `LocationCities_LocationCityID` INT NOT NULL,
-  `LocationCountries_LocationCountryID` INT NOT NULL,
+  `LocationRegions_LocationCity` VARCHAR(100) NOT NULL,
+  `LocationRegions_LocationCountry` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`LocationID`),
-  INDEX `fk_Locations_LocationCities1_idx` (`LocationCities_LocationCityID` ASC) VISIBLE,
-  INDEX `fk_Locations_LocationCountries1_idx` (`LocationCountries_LocationCountryID` ASC) VISIBLE,
-  CONSTRAINT `fk_Locations_LocationCities1`
-    FOREIGN KEY (`LocationCities_LocationCityID`)
-    REFERENCES `LittleLemonDB`.`LocationCities` (`LocationCityID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Locations_LocationCountries1`
-    FOREIGN KEY (`LocationCountries_LocationCountryID`)
-    REFERENCES `LittleLemonDB`.`LocationCountries` (`LocationCountryID`)
+  INDEX `fk_Locations_LocationRegions1_idx` (`LocationRegions_LocationCity` ASC, `LocationRegions_LocationCountry` ASC) VISIBLE,
+  CONSTRAINT `fk_Locations_LocationRegions1`
+    FOREIGN KEY (`LocationRegions_LocationCity` , `LocationRegions_LocationCountry`)
+    REFERENCES `LittleLemonDB`.`LocationRegions` (`LocationCity` , `LocationCountry`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`DeliveryStatuses`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`DeliveryStatuses` (
+  `DeliveryStatuses` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`DeliveryStatuses`))
 ENGINE = InnoDB;
 
 
@@ -129,13 +121,20 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Deliveries` (
   `DeliveryID` INT NOT NULL AUTO_INCREMENT,
   `DeliveryDate` VARCHAR(45) NULL,
-  `DeliveryStatus` VARCHAR(45) NULL,
+  `DeliveryCost` DECIMAL(5,2) NULL,
   `Locations_LocationID` INT NOT NULL,
+  `DeliveryStatuses_DeliveryStatuses` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`DeliveryID`),
   INDEX `fk_Deliveries_Locations1_idx` (`Locations_LocationID` ASC) VISIBLE,
+  INDEX `fk_Deliveries_DeliveryStatuses1_idx` (`DeliveryStatuses_DeliveryStatuses` ASC) VISIBLE,
   CONSTRAINT `fk_Deliveries_Locations1`
     FOREIGN KEY (`Locations_LocationID`)
     REFERENCES `LittleLemonDB`.`Locations` (`LocationID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Deliveries_DeliveryStatuses1`
+    FOREIGN KEY (`DeliveryStatuses_DeliveryStatuses`)
+    REFERENCES `LittleLemonDB`.`DeliveryStatuses` (`DeliveryStatuses`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -146,8 +145,10 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
   `OrderID` VARCHAR(11) NOT NULL,
-  `Quantity` VARCHAR(45) NULL,
-  `OrderCost` VARCHAR(45) NULL,
+  `Quantity` INT NULL,
+  `OrderCost` DECIMAL(6,2) NULL,
+  `OrderSales` DECIMAL(6,2) NULL,
+  `Discount` DECIMAL(5,2) NULL,
   `Deliveries_DeliveryID` INT NOT NULL,
   `Customers_CustomerID` VARCHAR(11) NOT NULL,
   PRIMARY KEY (`OrderID`),
@@ -167,14 +168,43 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `LittleLemonDB`.`MenuItemTypes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuItemTypes` (
+  `MenuItemType` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`MenuItemType`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`MenuItemCuisines`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuItemCuisines` (
+  `MenuItemCuisine` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`MenuItemCuisine`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `LittleLemonDB`.`MenuItems`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuItems` (
-  `MenuItemID` INT NOT NULL AUTO_INCREMENT,
-  `MenuItemName` VARCHAR(100) NULL,
-  `MenuItemType` VARCHAR(100) NULL,
-  `MenuItemCuisine` VARCHAR(100) NULL,
-  PRIMARY KEY (`MenuItemID`))
+  `MenuItemName` VARCHAR(100) NOT NULL,
+  `MenuItemTypes_MenuItemType` VARCHAR(100) NOT NULL,
+  `MenuItemCuisines_MenuItemCuisine` VARCHAR(100) NOT NULL,
+  INDEX `fk_MenuItems_MenuItemTypes1_idx` (`MenuItemTypes_MenuItemType` ASC) VISIBLE,
+  INDEX `fk_MenuItems_MenuItemCuisines1_idx` (`MenuItemCuisines_MenuItemCuisine` ASC) VISIBLE,
+  PRIMARY KEY (`MenuItemName`),
+  CONSTRAINT `fk_MenuItems_MenuItemTypes1`
+    FOREIGN KEY (`MenuItemTypes_MenuItemType`)
+    REFERENCES `LittleLemonDB`.`MenuItemTypes` (`MenuItemType`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_MenuItems_MenuItemCuisines1`
+    FOREIGN KEY (`MenuItemCuisines_MenuItemCuisine`)
+    REFERENCES `LittleLemonDB`.`MenuItemCuisines` (`MenuItemCuisine`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -182,13 +212,13 @@ ENGINE = InnoDB;
 -- Table `LittleLemonDB`.`Menu_Orders`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menu_Orders` (
+  `MenuItems_MenuItemName` VARCHAR(100) NOT NULL,
   `Orders_OrderID` VARCHAR(11) NOT NULL,
-  `Menu_MenuID` INT NOT NULL,
-  INDEX `fk_Menu_Orders_Menu1_idx` (`Menu_MenuID` ASC) VISIBLE,
+  INDEX `fk_Menu_Orders_MenuItems1_idx` (`MenuItems_MenuItemName` ASC) VISIBLE,
   INDEX `fk_Menu_Orders_Orders1_idx` (`Orders_OrderID` ASC) VISIBLE,
-  CONSTRAINT `fk_Menu_Orders_Menu1`
-    FOREIGN KEY (`Menu_MenuID`)
-    REFERENCES `LittleLemonDB`.`MenuItems` (`MenuItemID`)
+  CONSTRAINT `fk_Menu_Orders_MenuItems1`
+    FOREIGN KEY (`MenuItems_MenuItemName`)
+    REFERENCES `LittleLemonDB`.`MenuItems` (`MenuItemName`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Menu_Orders_Orders1`
